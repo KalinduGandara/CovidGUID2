7b38e6b53576690a168506bcdbfeed43fc30fdb2
thushalya <weeeratharava@gmail.com>
2021-11-18 22:18:15 +0530

    change profile page

--- views/profile.php
+++ views/profile.php
@@ -1,8 +1,21 @@
 <?php ?>
 <?php
-/** @var $this \app\core\View */
+///** @var $this \app\core\View */
 
 $this->title = "Profile"
 ?>
-<h1>Profile</h1>
 
+<div class="container">
+    <?php echo "<h1>$model->firstname $model->lastname</h1>" ?>
+    <?php $form = \app\core\form\Form::begin('', 'post') ?>
+
+    <?php echo $form->field($model, 'firstname') ?>
+
+    <?php echo $form->field($model, 'lastname') ?>
+
+    <?php echo $form->field($model, 'email') ?>
+    <?php echo $form->field($model, 'password')->passwordField() ?>
+    <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
+    <button type="submit" class="btn btn-primary">Update</button>
+</div>
+<?php \app\core\form\Form::end(); ?>
