03550c700f82ec38043ac351282c87b5102c4590
thushalya <weeeratharava@gmail.com>
2021-11-22 22:13:51 +0530

    home

--- views/components/category.php
+++ views/components/category.php
@@ -3,11 +3,6 @@
         <h3 class="panel-title"><?php echo $cat_title ?></h3>
     </div>
     <div class="panel-body">
-        <?php
-        foreach ($category_guidelines as $guideline) {
-            echo "* $guideline";
-            echo "<br>";
-        }
-        ?>
+
     </div>
 </div>
