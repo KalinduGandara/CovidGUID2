a0cc93f7e9dcba66703e96ccad387ab3e51d4d86
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2021-12-30 14:48:59 +0530

    Auto stash before checking out "HEAD"

--- views/search_sub_category.php
+++ views/search_sub_category.php
@@ -14,29 +14,10 @@
             <!-- Display Categories -->
 
             <?php
-            //            echo '<pre>';
-            //            var_dump($categories);
-            //            var_dump($guidelines);
-            //
-            //            echo '</pre>';
-            //            exit();
 
             foreach ($subcategories as $subcategory) {
-//                $cat_id = $category->cat_id;
-//                if ($subcategory['cat_id'] === $cat_id) {
-
-                    $sub_category_name = $subcategory->sub_category_name;
-                    //                    $cat_status = $category['cat_status'];
-
-                    $sub_category_id = $subcategory->sub_category_id;
-                    $sub_category_guidelines = [];
-                    foreach ($guidelines as $guideline) {
-                        if ($guideline->sub_category_id == $sub_category_id) {
-                            array_push($sub_category_guidelines, $guideline);
-                        }
-                    }
-                    include "components/subcategory.php";
-//                }
+                $subcategoryView = \app\views\components\subcategory\SubcategoryBuilder::buildPublicVeiw($subcategory->getSubCategoryId());
+                $subcategoryView->includeTitle()->render();
             }
             ?>
 
