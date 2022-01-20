7153342e3273a63bf67dd6823b4e5608d91ffab9
KalinduGandara <kalindugandara16@gmail.com>
2022-01-20 12:53:07 +0530

    add filter in admin users

--- views/admin_users.php
+++ views/admin_users.php
@@ -3,6 +3,52 @@
         <?php include "includes/admin_navigation.php" ?>
         <div class="col py-3 bg-white" style="margin-left: 250px">
             <?php if ($mode == 'show'){ ?>
+            <h1>Manage Users </h1>
+            <?php
+            $form = \app\core\form\Form::begin('', 'get');
+            $filter1 =  $form->selectField(null, 'status', [
+                '0' => 'Inactive',
+                '1' => 'Active',
+                '2' => 'Deleted',
+            ]);
+            $selectStatus = false;
+            $selectType = false;
+            if(isset($_GET['status'])){
+                $filter1->select($_GET['status']);
+                $selectStatus = true;
+            }
+            $filter2 =  $form->selectField(null, 'type', [
+                '1' => 'Officer',
+                '2' => 'Public User',
+            ]);
+            if(isset($_GET['type'])){
+                $filter2->select($_GET['type']);
+                $selectType = true;
+            }
+            ?>
+            <div class="row">
+                <div class="col">
+                    <label>Select Status</label>
+                    <?php echo $filter1; ?>
+                </div>
+                <div class="col">
+                    <label>Select Role</label>
+                    <?php echo $filter2; ?>
+                </div>
+            </div>
+            <div>
+                <?php if(((!isset($_GET['status'])) && (!isset($_GET['type'])))){?>
+                    <button type="submit" class="btn btn-success">Apply</button>
+                <?php }
+                else {?>
+                    <a href="/admin/users" class="btn btn-secondary">Clear filters</a>
+                <?php }?>
+                <a href="/admin/users?source=add_user" class="btn btn-primary">Add New User</a>
+            </div>
+            <?php
+            $form::end();
+            ?>
+            <hr>
 
             <div class="container-fluid">
                 <table class="table table-bordered table-hover">
@@ -18,7 +64,15 @@
                     <tbody>
 
                     <?php
-                    foreach (\app\models\User::getAll() as $user) {
+                    if ($selectType && $selectStatus)
+                        $users = \app\models\User::getAllWhere(['status'=>$_GET['status'],'type'=>$_GET['type']]);
+                    elseif ($selectType)
+                        $users = \app\models\User::getAllWhere(['type'=>$_GET['type']]);
+                    elseif ($selectStatus)
+                        $users = \app\models\User::getAllWhere(['status'=>$_GET['status']]);
+                    else
+                        $users = \app\models\User::getAll();
+                    foreach ($users as $user) {
                         $user_view = new \app\views\components\user\User($user);
                         $user_view->render();
                     }
